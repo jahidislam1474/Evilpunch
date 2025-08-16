e3db1d46d025adbef4551ef2531e6570cc028670
fluxxset <fluxx0303@gmail.com>
2025-08-17 02:03:05 +0530

    allowed host fix

--- evilpunch/evilpunch/settings.py
+++ evilpunch/evilpunch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*0e@94)uxjk+gd0yiq8d6)$z*c7eyq=!lkfed864oc%p4i_4ql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
